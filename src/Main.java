39949c7a9bc5d5ccb83f302f242c02cebbec2467
iShantaram <1agapovav987@gmail.com>
2022-10-23 14:58:57 +0300

    Homework 11, chapter 9 (strings) completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // HW11
+        // Task1
+        System.out.println("\nTask 1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Task2
+        System.out.println("\nTask 2:");
+
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperCaseFullName);
+
+        // Task3
+        System.out.println("\nTask 3:");
+        fullName = "Иванов Семён Семёнович";
+        String notYoFullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + notYoFullName);
+
     }
 }
